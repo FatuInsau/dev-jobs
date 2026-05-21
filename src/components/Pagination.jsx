e6def6cfcdf450970bb2389ca2f77d0e983ae16b
FatuInsau <insaufaty@gmail.com>
2026-05-21 00:07:41 -0300

    Se agrega Link componente

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,3 +1,5 @@
+import { Link } from "./Link"
+
 export function Pagination ({ currentPage = 1, totalPages = 5, onPageChange = () => {}}) {
 
     const pages = Array.from({ length: totalPages }, (_, i) => i+1)
@@ -31,28 +33,28 @@ export function Pagination ({ currentPage = 1, totalPages = 5, onPageChange = ()
 
     return (
         <nav className="pagination-nav">
-            <a href="#" className={stylePrevButton} onClick={handlePrev}>
+            <Link href="#" className={stylePrevButton} onClick={handlePrev}>
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
           strokeLinecap="round" strokeLinejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M15 6l-6 6l6 6" />
         </svg>
-            </a>
+            </Link>
             { pages.map((page) => {
                 return (
-                    <a key={page} className={currentPage === page ? 'is-active' : ''} href="#" onClick={(e)=>{handlePageClick(e,page)}}>
+                    <Link key={page} className={currentPage === page ? 'is-active' : ''} href="#" onClick={(e)=>{handlePageClick(e,page)}}>
                         {page}
-                    </a>
+                    </Link>
                 )
             })}
-            <a href="#" className={styleNextButton} onClick={handleNext}>
+            <Link href="#" className={styleNextButton} onClick={handleNext}>
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5"
           strokeLinecap="round" strokeLinejoin="round"
           className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right">
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M9 6l6 6l-6 6" />
         </svg>
-            </a>
+            </Link>
         </nav>
     )
 }
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "../components/Link"
 export function NotFoundPage () {
   return (
     <>
@@ -6,7 +6,9 @@ export function NotFoundPage () {
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-error-404"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 8v3a1 1 0 0 0 1 1h3" /><path d="M7 8v8" /><path d="M17 8v3a1 1 0 0 0 1 1h3" /><path d="M21 8v8" /><path d="M10 10v4a2 2 0 1 0 4 0v-4a2 2 0 1 0 -4 0" /></svg>
             <h1>Página no encontrada</h1>
             <p>Lo sentimos, la página que buscas no existe.</p>
-            <button>Volver al Inicio</button>
+            <Link href="/"><button>Volver al Inicio</button>
+            </Link>
+            
         </main>
     </>
     
