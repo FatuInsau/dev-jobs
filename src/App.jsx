101707dc3530bd18e8d3d140a11192c635190984
FatuInsau <insaufaty@gmail.com>
2026-06-10 19:19:22 -0300

    Optimización al cargar las páginas (lazy)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import { lazy, Suspense } from 'react'
 import { Routes, Route } from 'react-router'
+
 import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
-import { SearchPage } from "./pages/Search"
-import { Home } from "./pages/Home"
-import { NotFoundPage } from "./pages/NotFoundPage"
-import { JobDetail } from './pages/JobDetail'
+
+const Home = lazy(()=> import("./pages/Home"))
+const SearchPage = lazy(()=> import("./pages/Search"))
+const NotFoundPage = lazy(()=> import("./pages/NotFoundPage"))
+const JobDetail = lazy(()=> import("./pages/JobDetail"))
 
 function App() {
 
   return (
     <>
     <Header></Header>
+    <Suspense fallback={<div style={{maxWidth: '1280px', margin:'0 auto', padding: '1rem'}}><p style={{margin:'0 auto', padding: '2rem'}}>
+      Cargando...
+    </p></div>}>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/search" element={<SearchPage />} />
@@ -18,6 +24,7 @@ function App() {
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
     <Footer></Footer>
+    </Suspense>
     </>
   )
 }
